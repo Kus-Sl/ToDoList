add1b1de4b75150f122d1e058e5973921ba505e1
Kusakin_Sl <kusakin.slava@inbox.ru>
2022-07-15 00:38:52 +0300

    Integration of CoreData

---
+++ ToDoList/ServiceLayers/StorageManager.swift
@@ -0,0 +1,78 @@
+//
+//  StorageManager.swift
+//  ToDoList
+//
+//  Created by Вячеслав Кусакин on 14.07.2022.
+//
+
+import Foundation
+import CoreData
+
+
+class StorageManager {
+
+    static let shared = StorageManager()
+
+    private let entityName = "ToDoTask"
+
+    private lazy var context = persistentContainer.viewContext
+
+    private lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "ToDoList")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    private init() {}
+
+    func fetchData() -> [ToDoTask]? {
+        let fetchRequest = ToDoTask.fetchRequest()
+
+        do {
+            return try context.fetch(fetchRequest)
+        } catch let error {
+            print("Failed to fetch data", error)
+            return nil
+        }
+    }
+
+    func saveContext () {
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let error = error as NSError
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+    }
+
+    func saveNewTask(_ taskTitle: String) {
+        guard let entityDescription = NSEntityDescription.entity(forEntityName: entityName, in: context) else { return }
+        guard let toDoTask = NSManagedObject(entity: entityDescription, insertInto: context) as? ToDoTask else { return }
+
+        toDoTask.title = taskTitle
+
+        saveContext()
+    }
+
+    func deleteTask(_ task: ToDoTask) {
+        context.delete(task)
+
+        saveContext()
+    }
+
+    func clearContext() {
+        guard let tasks = fetchData() else { return }
+        for task in tasks {
+            context.delete(task)
+        }
+        
+        saveContext()
+    }
+}
+
--- ToDoList/ViewControllers/TaskListViewController.swift
+++ ToDoList/ViewControllers/TaskListViewController.swift
@@ -15,8 +15,7 @@ class TaskListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        taskList = ToDoTask.createTasks()
+        taskList = StorageManager.shared.fetchData() ?? []
     }
 
     @IBAction func AddNewTask(_ sender: UIBarButtonItem) {
@@ -26,16 +25,17 @@ class TaskListViewController: UITableViewController {
     private func createTask() {
         let alert = UIAlertController(title: "New task", message: nil, preferredStyle: .alert)
 
-        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
-            guard let task = alert.textFields?.first?.text, !task.isEmpty else { return }
-
-            self.taskList.insert(ToDoTask(title: task), at: 0)
-
-            let cellIndex = IndexPath(row: 0, section: 0)
-            self.tableView.insertRows(at: [cellIndex], with: .automatic)
-        }
-
         let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+
+        let saveAction = UIAlertAction(title: "Save", style: .default) { [self] _ in
+            guard let taskTitle = alert.textFields?.first?.text, !taskTitle.isEmpty else { return }
+
+            StorageManager.shared.saveNewTask(taskTitle)
+            taskList = StorageManager.shared.fetchData() ?? []
+
+            let cellIndex = IndexPath(row: taskList.count - 1, section: 0)
+            tableView.insertRows(at: [cellIndex], with: .automatic)
+        }
 
         alert.addAction(saveAction)
         alert.addAction(cancelAction)
@@ -64,6 +64,7 @@ extension TaskListViewController {
 
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
+            StorageManager.shared.deleteTask(taskList[indexPath.row])
             taskList.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .automatic)
         }
@@ -74,20 +75,6 @@ extension TaskListViewController {
 extension TaskListViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-    }
-}
-
-struct ToDoTask {
-    let title: String
-
-    static func createTasks() -> [ToDoTask] {
-        [ToDoTask(title: "shower"),
-         ToDoTask(title: "dinner"),
-         ToDoTask(title: "walk"),
-         ToDoTask(title: "study"),
-         ToDoTask(title: "run"),
-         ToDoTask(title: "music"),
-         ToDoTask(title: "sleep"),]
     }
 }
 
